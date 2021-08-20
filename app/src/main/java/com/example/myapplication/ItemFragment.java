7b412b70f5cec7fe3c2a6dc6eca03fd149bc2771
0JIEUN0 <jieun4436@naver.com>
2021-08-21 00:03:33 +0900

    Add ItemFragment

--- app/src/main/java/com/example/myapplication/ItemFragment.java
+++ app/src/main/java/com/example/myapplication/ItemFragment.java
@@ -2,36 +2,26 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import com.example.myapplication.databinding.FragmentLikeBinding;
 
-public class LikeFragment extends Fragment {
+import com.example.myapplication.databinding.FragmentItemBinding;
 
-    private FragmentLikeBinding binding = null;
+public class ItemFragment extends Fragment {
+    private FragmentItemBinding binding = null;
 
-    public LikeFragment() {
+    public ItemFragment() {
         // Required empty public constructor
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        binding = FragmentLikeBinding.inflate(getLayoutInflater(), container, false);
+        binding = FragmentItemBinding.inflate(getLayoutInflater(), container, false);
         return binding.getRoot();
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        // code
     }
 
     @Override
