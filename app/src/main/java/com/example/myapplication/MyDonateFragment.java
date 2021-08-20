af5f6f97146204142041c6427f62f8f1ded89c06
0JIEUN0 <jieun4436@naver.com>
2021-08-21 04:37:30 +0900

    MyPageFragment: Make Fragment Transaction to MyDonateFragment

--- app/src/main/java/com/example/myapplication/MyDonateFragment.java
+++ app/src/main/java/com/example/myapplication/MyDonateFragment.java
@@ -5,23 +5,25 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import com.example.myapplication.databinding.FragmentMyPageBinding;
 
-public class MyPageFragment extends Fragment {
+import com.example.myapplication.databinding.FragmentMyDonateBinding;
 
-    private FragmentMyPageBinding binding = null;
+public class MyDonateFragment extends Fragment {
+    FragmentMyDonateBinding binding = null;
 
-    public MyPageFragment() {
+    public MyDonateFragment() {
         // Required empty public constructor
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        binding = FragmentMyPageBinding.inflate(getLayoutInflater(), container, false);
+        binding = FragmentMyDonateBinding.inflate(getLayoutInflater(), container, false);
         return binding.getRoot();
     }
 
@@ -29,18 +31,14 @@ public class MyPageFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        // code
         initListener();
     }
 
     private void initListener() {
-        binding.donateInfoBtn.setOnClickListener(new View.OnClickListener() {
+        binding.backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Fragment transaction to donate details page
-                ((MainActivity)getActivity()).fragmentTranslation(
-                    new MyDonateFragment(), true
-                );
+                getActivity().onBackPressed();
             }
         });
     }
@@ -50,4 +48,5 @@ public class MyPageFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
 }
